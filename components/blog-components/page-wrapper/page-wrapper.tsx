65daf46ebc34dddf13276a33711781012aeb0d43
Stanislav <70153732+Stazzz544@users.noreply.github.com>
2023-03-25 23:08:38 +0300

    fix container

--- components/blog-components/page-wrapper/page-wrapper.tsx
+++ components/blog-components/page-wrapper/page-wrapper.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page-wrapper.module.scss";
 import cs from "classnames";
 import { ReactNode } from "react";
-import Container from "@/components/container/container";
+import Container from "../container/container";
 
 type PageWrapperProps = {
   src: string;
